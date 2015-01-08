60b89904c3cf4c18a7b72d89aa818cd947f5e04f
Giuseppe Mazzotta <gdm85@users.noreply.github.com>
2015-01-08 15:52:17 +0100

    Always use my fork (stable versions only)

--- docker-ipv4/build.sh
+++ docker-ipv4/build.sh
@@ -2,7 +2,7 @@
 export PATH="$PATH:/usr/local/go/bin"
 export GOPATH=~/goroot
 
-go get "github.com/fsouza/go-dockerclient" || exit $?
+go get "github.com/gdm85/go-dockerclient" || exit $?
 
 ## build without debug information
 go build -ldflags "-w -s"
